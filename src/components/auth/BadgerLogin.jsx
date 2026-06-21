7c32fdc7a9906bba1ae5d2ac78465875a21bb5a9
Qingsong Hu <kingsanhu@gmail.com>
2026-06-21 18:11:50 +0800

    Finish step 5: Allow Login

--- src/components/auth/BadgerLogin.jsx
+++ src/components/auth/BadgerLogin.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export default function BadgerLogin() {
-
     // TODO Create the login component.
+    const usernameRef = useRef();
+    const passwdRef = useRef();
 
-    return <>
-        <h1>Login</h1>
-    </>
+    function validateLoginInfo() {
+        if (
+            usernameRef.current.value === "" ||
+            passwdRef.current.value === ""
+        ) {
+            alert("You must provide both a username and pin!");
+            return false;
+        }
+        if (!/^\d{7}$/.test(passwdRef.current.value)) {
+            alert("Your pin is a 7-digit number!");
+            return false;
+        }
+        return true;
+    }
+
+    function handleLogin(e) {
+        e?.preventDefault();
+        if (validateLoginInfo()) {
+            fetch("https://cs571.org/rest/s25/hw6/login", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "X-CS571-ID": CS571.getBadgerId(),
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: usernameRef.current.value,
+                    pin: passwdRef.current.value,
+                }),
+            }).then((res) => {
+                if (res.status === 401) {
+                    alert("Incorrect username or pin!");
+                } else if (res.status === 200) {
+                    alert("Login successfully!");
+                }
+            });
+        }
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+            <Form onSubmit={handleLogin}>
+                <Form.Label htmlFor="username">Username</Form.Label>
+                <Form.Control id="username" ref={usernameRef} />
+                <br />
+                <Form.Label htmlFor="passwd">Password</Form.Label>
+                <Form.Control id="passwd" ref={passwdRef} type="password" />
+                <br />
+                <Button type="submit">Login</Button>
+            </Form>
+        </>
+    );
 }
